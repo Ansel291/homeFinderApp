a9a2719768fd6aee6b48bbdc1f6fe8c00eab190a
jason <jason@herman-scheer.com>
2023-10-24 11:50:08 -1000

    try to fix

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -31,7 +31,7 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
     price,
     type,
   } = listing
-  console.log('aaa')
+  console.log('bbb')
   //console.log(id)
   return (
     <>
@@ -44,7 +44,7 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
               }}
             >
               <AiFillDelete
-                className='cursor-pointer relative float-right ml-[20px] mb-[10px] transition'
+                className='cursor-pointer z-[99999999999999] relative float-right ml-[20px] mb-[10px] transition'
                 //fill='rgb(231,76,60)'
                 //fill='#a7a7a7'
                 onMouseEnter={() => setHoverDelete(true)}
@@ -73,7 +73,7 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
                 //onClickCapture={(e) => {
                 onClick={(e) => {
                   e.preventDefault()
-                  e.stopPropagation()
+                  //e.stopPropagation()
                   onEdit(id)
                 }}
               />
