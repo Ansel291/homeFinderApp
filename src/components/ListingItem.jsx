3ef4fef7b07829db34e4fa16770b724aa64b167c
jason <jason@herman-scheer.com>
2023-10-23 17:18:52 -1000

    try to fix

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -50,7 +50,7 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
                 onMouseEnter={() => setHoverDelete(true)}
                 onMouseLeave={() => setHoverDelete(false)}
                 fill={hoverDelete ? '#f25757' : '#a7a7a7'}
-                onPointerEnter={(e) => {
+                onClick={(e) => {
                   e.preventDefault()
                   onDelete(id, name)
                 }}
@@ -69,7 +69,7 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
                 onMouseEnter={() => setHoverEdit(true)}
                 onMouseLeave={() => setHoverEdit(false)}
                 fill={hoverEdit ? '#f97316' : '#a7a7a7'}
-                onPointerEnter={(e) => {
+                onClick={(e) => {
                   e.preventDefault()
                   onEdit(id)
                 }}
