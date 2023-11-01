2a1fbd7c4f95e70eba04d3c6b7314cff69dbef71
jason <jason@herman-scheer.com>
2023-11-01 12:06:49 -1000

    trying to fix

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -31,7 +31,7 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
     price,
     type,
   } = listing
-  console.log('aaa')
+  console.log('bbb')
   //console.log(id)
   return (
     <>
@@ -41,8 +41,8 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
             className='cursor-pointer float-right ml-[20px] mb-[10px] mr-[50px]'
             //onClick={(e) => {
             onPointerEnter={(e) => {
-              e.persist()
-              //e.preventDefault()
+              //e.persist()
+              e.preventDefault()
               //e.stopPropagation()
               onDelete(id, name)
             }}
@@ -55,7 +55,7 @@ function ListingItem({ listing, id, onDelete, onEdit }) {
             className='cursor-pointer float-left mb-[7.5px] mr-[6px] top-[1px]'
             //onClick={(e) => {
             onPointerEnter={(e) => {
-              //e.preventDefault()
+              e.preventDefault()
               //e.stopPropagation()
               onEdit(id)
             }}
